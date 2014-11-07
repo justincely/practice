aba7f22daaf55236cd150fb14d445f70f03ca33d
Justin Ely <ely@stsci.edu>
2014-11-06 20:54:54 -0500

    solved problem 1 in ruby

--- project_euler/euler.rb
+++ project_euler/euler.rb
@@ -3,6 +3,22 @@
 require 'functions.rb'
 require 'mathn'
 
+def problem_1(n)
+  sum = 0
+
+  for i in 0...n
+    if i % 3 == 0
+      sum += i
+    elsif i % 5 == 0
+      sum += i
+    end
+  end
+
+  return sum
+end
+
+
+
 def problem_3(num=600851475143)
   max_prime = 1
 
@@ -18,5 +34,5 @@ end
 
 
 
-
+puts "Problem 1:  #{problem_1(1000)}"
 puts "Problem 3:  #{problem_3}"
